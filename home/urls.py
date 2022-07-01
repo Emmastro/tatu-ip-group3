4f6e9e8e0c2c04c923f361627917758a6f6f6f88
Virgo Alpha <bensonkingori96@gmail.com>
2022-07-01 12:29:01 +0300

    home page update

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	path('aboutUs/', views.aboutUs, name='aboutUs'),
+	path('posts/', views.posts, name='posts'),
+	path('team/', views.team, name='team'),
+	path('contact/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def aboutUs(request):
+    return render(request, 'aboutUs.html')
+
+def posts(request):
+    return render(request, 'posts.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def contact(request):
+    return render(request, 'contact.html')
